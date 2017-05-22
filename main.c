b76f8bf044d79a7364becd0ec36c05d5b824f3bd
frajdad <frajdzioszek.jierda@gmail.com>
2017-05-22 22:28:11 +0200

    w main jest zarys menu glownego programu i trybu interaktywnego

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+
 
 int main (int argc, char ** argv) {
-	printf("%s\n", argv[0]);
+	char znak;
+    printf("Menu glowne:\n\n"
+                   "Wybierz:\n"
+                   "1 - wystawienie paragonu\n");
+    znak = getch();
+    if (znak == 49){
+        printf("Wchodzisz w menu paragonu\n");
+    else printf("Bledna komenda\n");
+    Sleep(5000);
 	return 0;
 }
--- odczyt.c
+++ odczyt.c
@@ -3,6 +3,7 @@
 void odczyt (FILE * plik, float ** tab) {
 	fopen (plik, "r");
 	while (fscanf (plik, "%f", &tab[i]) != EOF)
+		printf("%f\n", tab[i]);
 		i++;
 	fclose(plik);
 }
