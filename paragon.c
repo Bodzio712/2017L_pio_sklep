2fa0b13c422d74a9126f02daabc218aa8848dd46
frajdad <frajdzioszek.jierda@gmail.com>
2017-05-27 21:16:16 +0200

    poprawione jeszcze bardziej, dzialaja podstawy

--- paragon.c
+++ paragon.c
@@ -4,24 +4,20 @@
 
 #include <stdio.h>
 #include <conio.h>
-#include <string.h>
 
-void WypiszProdukt(int kod);
+float WypiszProdukt(int kod);
 
-void WypiszProdukt(int kod){
+float WypiszProdukt(int kod){
     FILE * dane = fopen("BazaDanych.txt","r");
 
     int kodzik;
     char nazwa[100];
     float cena;
-    printf("checkpoint1\n");
-    while (fscanf(dane, "%d %s %f", &kodzik, &nazwa[0], &cena) != EOF){
-        printf("checkpoint1,5\n");
-        if (kodzik==kod){
-            printf("checkpoint2\n");
-            printf("%d %s %f", kodzik, nazwa, cena);
+    while (fscanf(dane, "%s %d %f", &nazwa, &kodzik, &cena) != EOF){
+        if (kod==kodzik) {
+            printf("%d %s %.2f\n", kodzik, nazwa, cena);
+            return cena;
         }
-        else printf("cos nie pyklo");
     }
 }
 
@@ -30,12 +26,21 @@ void Paragon(){
     FILE * baza = fopen("BazaDanych.txt","r");
     //char * nazwaParagonu = "0012.txt";
     //FILE * paragon = fopen("0012.txt","w");
-    int kod;
+    int kod, ilosc;
+    float cena;
+    float suma = 0;
+    float zaplata;
 
     while(getch() != 27) {
-        printf("Wpisz kod produktu:\n");
-        scanf("%d", &kod);
+        printf("Wpisz kod produktui jego ilosc:\n");
+        scanf("%d %d", &kod, &ilosc);
         printf("\n");
-        WypiszProdukt(kod);
+        cena = WypiszProdukt(kod) * ilosc;
+        suma += cena;
     }
+    printf("Do zaplaty: %f\n", suma);
+    printf("Zaplacono:");
+    scanf("%f", &zaplata);
+    printf("\nReszta: %f\n",zaplata - suma);
+    return;
 }
