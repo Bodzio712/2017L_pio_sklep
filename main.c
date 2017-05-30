041a18c7215e474c5a022c1860cc7e63cefffcf6
frajdad <frajdzioszek.jierda@gmail.com>
2017-05-30 21:51:03 +0200

    test paragonu zrobiony - działa

--- main.c
+++ main.c
@@ -5,9 +5,18 @@
 #include "paragon.h"
 
 void menu();
+void test();
+
+float zaokraglij(float x)
+{
+    int y = x * 1000; // przesuwamy przecinek o 4 miejsca i pozbywamy sie reszty za przecinkiem - y jest calkowite
+    if (y % 10 >= 5) y += 10; // jezeli cyfra jednosci >= 5
+    return (y / 10) * 0.01; // usuwamy ostatnia cyfre i zamieniamy na liczbe zmiennoprzecinkowa
+}
 
 int main (int argc, char ** argv) {
     menu();
+    test();
     return 0;
 }
 
@@ -28,3 +37,54 @@ void menu(){
     }
     Sleep(5000);
 }
+
+void test(){
+    //w konsoli testujemy kupno 2 produktów z kodem 1, 4 z kodem 5
+    // oraz 1 z kodem 15 - nie ma takiego kodu
+    char test[50];
+    char test2[50];
+    char test3[50];
+    char test4[50];
+    int ilosc;
+    int wskaznik = 0;
+    float cena, suma;
+    float cena1 = 2.19, cena2 = 14.99;
+    float suma1 = 4.38, suma2 = 59.96, suma3 = 64.34;
+    FILE * paragon = fopen("C:\\Users\\Damian\\CLionProjects\\2017L_pio_sklep\\cmake-build-debug\\0012.txt","r");
+    fscanf(paragon,"%s %s", &test, &test2);
+    printf("%s \n",test);
+    fscanf(paragon,"%s %s %s %s",&test, &test2, &test3, &test4);
+    printf("%s %s %s %s\n", test, test2, test3, test4);
+    if(strcmp(test,"Nazwa")==0 && strcmp(test2,"Cena/jedn")==0 && strcmp(test3,"Ilosc")==0 && strcmp(test4,"Cena")==0)
+        wskaznik += 1;
+    fscanf(paragon, "%s %f %d %f", &test, &cena, &ilosc, &suma);
+    printf("%s %f %d %f\n", test, cena, ilosc, zaokraglij(suma));
+    if (strcmp(test, "mleko")==0) {
+        if (cena == cena1) {
+            if (ilosc == 2) {
+                if (zaokraglij(suma) == suma1) {
+                    wskaznik += 1;
+                }
+            }
+        }
+    }
+    fscanf(paragon, "%s %f %d %f", &test, &cena, &ilosc, &suma);
+    printf("%s %f %d %f\n", test, cena, ilosc, zaokraglij(suma));
+    if (strcmp(test, "papierosy")==0) {
+        if (cena == cena2) {
+            if (ilosc == 4) {
+                if (zaokraglij(suma) == suma2) {
+                    wskaznik += 1;
+                }
+            }
+        }
+    }
+    fscanf(paragon,"%s %f", &test, &suma);
+    printf("%s %f\n", test, zaokraglij(suma));
+    if (strcmp(test,"SUMA:")==0 && zaokraglij(suma) == suma3)
+        wskaznik+=1;
+    if (wskaznik==4)
+        printf("Zgadza sie\n");
+    Sleep(5000);
+    return;
+};
--- paragon.c
+++ paragon.c
@@ -5,20 +5,21 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <time.h>
 
-float WypiszProdukt(int kod);
-
-float WypiszProdukt(int kod){
-    FILE * dane = fopen("BazaDanych.txt","r");
+float WypiszProdukt(FILE * paragon, int kod){
+    FILE * dane = fopen("C:\\Users\\Damian\\CLionProjects\\2017L_pio_sklep\\BazaDanych.txt","r");
 
     int kodzik;
     char nazwa[100];
     float cena;
     while (fscanf(dane, "%s %d %f", &nazwa, &kodzik, &cena) != EOF){
-        if (kod==kodzik) {
+        if (kodzik==kod) {
             printf("%d ", kodzik);
             printf("%s ", nazwa);
             printf("%.2f\n", cena);
+            fprintf(paragon, "%15s ", nazwa);
+            fprintf(paragon, "%8.2f ", cena);
             return cena;
         }
     }
@@ -28,21 +29,33 @@ float WypiszProdukt(int kod){
 
 void Paragon(){
 
-    //FILE * baza = fopen("BazaDanych.txt","r");
     //char * nazwaParagonu = "0012.txt";
-    //FILE * paragon = fopen("0012.txt","w");
+    FILE * paragon = fopen("0012.txt","w");
     int kod, ilosc;
     float cena;
     float suma = 0;
     float zaplata;
 
-    while(getch() != 122) {
-        printf("Wpisz kod produktui jego ilosc:\n");
+    time_t czas;
+    struct tm * data;
+    char godzina[ 80 ];
+
+    time( & czas );
+    data = localtime( & czas );
+
+    strftime( godzina, 80, "%c", data );
+    fprintf(paragon, "%s\n", godzina);
+    fprintf(paragon, "%15s  Cena/jedn Ilosc    Cena\n", "Nazwa");
+    while(getch() != 27) {
+        printf("Wpisz kod produktu i jego ilosc:\n");
         scanf("%d %d", &kod, &ilosc);
-        cena = WypiszProdukt(kod) * ilosc;
+        cena = WypiszProdukt(paragon, kod) * ilosc;
         printf("%.2f\n",cena);
+        if (cena != 0)
+            fprintf(paragon, "%6d\t%8.2f\n", ilosc, cena);
         suma += cena;
     }
+    fprintf(paragon, "SUMA: %.2f\n", suma);
     printf("Do zaplaty: %.2f zlotych\n", suma);
     printf("Zaplacono:");
     while(1<2) {
@@ -56,4 +69,5 @@ void Paragon(){
             suma = suma - zaplata;
         }
     }
+    fclose(paragon);
 }
